53889d97f2074646dce89e62a6952a6091288efe
Garen Hu <a514832141@gmail.com>
2023-11-27 22:54:37 -0600

    add server

---
+++ server/server_test.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"fmt"
+	"gogocache/cache"
+	"log"
+	"net/http"
+	"testing"
+)
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func TestServer(t *testing.T) {
+	cache.NewGroup("scores", 2<<10, cache.GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	addr := "localhost:9999"
+	peers := NewHTTPPool(addr)
+	log.Println("geecache is running at", addr)
+	http.ListenAndServe(addr, peers)
+}
